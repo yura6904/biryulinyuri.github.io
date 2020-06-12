36a7d269aa2f315c6e60d17276c4626fb46cba23
yura6904 <biryulinyura@mail.ru>
2020-06-12 17:55:09 +0300

    added build

---
+++ precache-manifest.7c8958c98246b17c9d2bd98bbaf13088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817b056ba83837cf4533a79d1bb738a0",
+    "url": "/biryulinyuriTestApp/index.html"
+  },
+  {
+    "revision": "af037f1d7bdf9425e233",
+    "url": "/biryulinyuriTestApp/static/css/main.cbb2ece8.chunk.css"
+  },
+  {
+    "revision": "e43171c8e8e0a485bf8b",
+    "url": "/biryulinyuriTestApp/static/js/2.881ed088.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/biryulinyuriTestApp/static/js/2.881ed088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af037f1d7bdf9425e233",
+    "url": "/biryulinyuriTestApp/static/js/main.baefb6d0.chunk.js"
+  },
+  {
+    "revision": "5fefa5364fac8ae5cd9d",
+    "url": "/biryulinyuriTestApp/static/js/runtime-main.9a3625ef.js"
+  },
+  {
+    "revision": "4b71f8137985eaa992d17a315997791e",
+    "url": "/biryulinyuriTestApp/static/media/user.4b71f813.png"
+  }
+]);
